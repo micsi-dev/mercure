5cee86e1512f35cfb11e098acb9700c16f02048c
Todd Etzel <todd@highlandbiomed.com>
2026-01-08 13:01:21 -0500

    We don't need these any more, just using module settings JSON

--- app/common/types.py
+++ app/common/types.py
@@ -189,9 +189,6 @@ class Module(BaseModel, Compat):
     requires_persistence: Optional[bool] = False
     persistence_folder_name: Optional[str] = ""
     network_mode: Optional[str] = "bridge"  # "none", "bridge", or custom network name
-    require_signature: Optional[bool] = False  # Require Sigstore/Cosign signature verification
-    signature_certificate_identity: Optional[str] = ""  # Expected certificate identity (email/URL)
-    signature_certificate_oidc_issuer: Optional[str] = ""  # Expected OIDC issuer (e.g., https://github.com/login/oauth)
 
 
 class UnsetRule(TypedDict):
